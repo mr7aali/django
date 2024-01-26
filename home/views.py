7e1c7c485940ff4443b2f0b05d7c7b54cad5f6a3
mr7aali <mr7aali@gmail.com>
2024-01-26 13:56:41 +0600

    render html as response

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+# def home(request):
+#     return HttpResponse("Hey i want to free palestine")
+
+
+def home(request):
+    return render(request, "index.html")
